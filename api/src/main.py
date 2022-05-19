2c6e2ab2554120ff7a4799ac0c57f16a615405ee
Tshepiso Mogapi <tshepzmogapi@gmail.com>
2022-05-20 00:50:42 +0200

    add dockerfile path

--- api/src/main.py
+++ api/src/main.py
@@ -5,7 +5,7 @@ import logging
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/visitors")
 async def read_root(request: Request):
     logging.info(f'Incoming visitor from {request.client.host}')
     return {"Hello there visitor from": request.client.host}
