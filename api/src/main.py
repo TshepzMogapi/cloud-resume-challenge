70a6ec6b4fad4952483dd7d5806041293284cb42
Tshepiso Mogapi <tshepzmogapi@gmail.com>
2022-05-24 23:07:16 +0200

    different verion

--- api/src/main.py
+++ api/src/main.py
@@ -2,10 +2,9 @@ from datetime import datetime as dt
 from fastapi import FastAPI, Request
 import logging
 
-
 app = FastAPI()
 
 @app.get("/visitors")
 async def read_root(request: Request):
-    logging.info(f'Incoming visitor from {request.client.host}')
+    logging.debug(f'Incoming visitor from {request.client.host}')
     return {"ip": request.client.host,"date": dt.now()}
