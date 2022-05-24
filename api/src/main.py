370d0bf18ce69fc28e4a04e5c6345df64de88c98
Tshepiso Mogapi <tshepzmogapi@gmail.com>
2022-05-24 02:24:43 +0200

    fix docker file and return json objects as response

--- api/src/main.py
+++ api/src/main.py
@@ -1,4 +1,4 @@
-
+from datetime import datetime as dt
 from fastapi import FastAPI, Request
 import logging
 
@@ -8,4 +8,4 @@ app = FastAPI()
 @app.get("/visitors")
 async def read_root(request: Request):
     logging.info(f'Incoming visitor from {request.client.host}')
-    return {"Hello there visitor from": request.client.host}
+    return {"ip": request.client.host,"date": dt.now()}
